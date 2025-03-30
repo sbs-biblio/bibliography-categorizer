cb679b2a168a69326066b72a498af84140145ef9
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-30 17:25:52 +0200

    Add 2x redundancy by matching user's uuid and also next uuid in the list

--- script.js
+++ script.js
@@ -25,6 +25,28 @@ let allReferences = [];
 let filteredReferences = [];
 let currentIndex = 0;
 let uuid = null;
+let uuids = [
+    "a02e35b9-3620-4021-ab4a-acab6baa134a",
+    "efba1e33-d566-476c-a042-14d94c084b62",
+    "2291e0bc-2eb3-4d29-8c3d-eb38875aecdd",
+    "42dde4a9-cb1b-4ea4-ab37-a5c14d341cb2",
+    "e2eaba9d-4367-4162-ac71-152e824186e4",
+    "c1482df3-0324-47c1-9d2e-d9453778272e",
+    "c3306b65-3c6e-4bb5-9622-adb45c7f8c65",
+    "6214f6c3-1c68-4fd5-832d-af072afe4d43",
+    "d44da73a-4061-4258-a663-d31a13b98493",
+    "059fef4f-5346-4b82-8318-35811f250831",
+    "245430f2-ab7b-499d-b735-c71c3ca94d73",
+    "c02c6ca3-b434-4400-ac6e-97c9430b5c0f",
+    "e8570cec-c05a-4705-a7a0-286c73393e19",
+    "75d0051c-1c45-47a7-b91a-05300dc81bac",
+    "5cf91236-70b5-4a4a-9422-dcd355ffbb98",
+    "15b400c0-cbb8-454c-ba2c-6b824bbcd93d",
+    "db5c956f-0f72-4e61-848d-33b41ec68e20",
+    "5df7392a-3293-41df-931d-33c2e34aa825",
+    "eb232884-27bf-4bc9-862b-dea1bf6079d3",
+    "3674c2fb-159e-413c-adc4-be3c69dc6841"
+];
 let classifications = {};
 
 const sheetEndpoint = "https://script.google.com/macros/s/AKfycbx-bdRmz-7Rz6HhG-2cnkUmgYDvQ2gZqR_BR7YV5UYmOm43jA-7fBRbOZSI2RzhlOCwTg/exec";
@@ -45,8 +67,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     allReferences = allReferences.filter(ref => ref.pdf && ref.pdf.trim() !== "");
 
     // Filter by UUID if provided
+    // NOTE: redundancy, will match all references with the current UUID OR the next UUID, for
+    // redundancy.
     filteredReferences = uuid
-        ? allReferences.filter(ref => ref.group === uuid)
+        ? allReferences.filter(ref => {
+            const currentIndex = uuids.indexOf(uuid);
+            const nextIndex = (currentIndex + 1) % uuids.length;
+            const nextUuid = uuids[nextIndex];
+            return ref.group === uuid || ref.group === nextUuid;
+        })
         : allReferences;
 
     loadClassificationsFromStorage();
